734c65cd42fd7d15e7884a866ce2735aae3a9f7e
Anton Korzh <antonkrzh@outlook.com>
2020-09-23 16:31:07 +0300

    Bind BHF parsing to the API

--- backend/urls.py
+++ backend/urls.py
@@ -3,15 +3,17 @@ from collections import namedtuple
 from typing import List
 from flask_restful import Api
 
+import resources
 
-APIResource = namedtuple('URLPattern', ('resource', 'endpoint'))
+
+APIResource = namedtuple('APIResource', ('resource', 'endpoint'))
 
 
 urlpatterns: List[APIResource] = [
-
+    APIResource(resources.BHFMessages, '/messages/bhf')
 ]
 
 
 def apply_resources(api: Api):
     for res in urlpatterns:
-        api.add_resource(res.resource, endpoint=res.endpoint)
+        api.add_resource(res.resource, res.endpoint)
